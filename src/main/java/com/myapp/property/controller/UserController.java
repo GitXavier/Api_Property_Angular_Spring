c203dbbcfb3a13b0d4e218fcd324a50c26240564
Xavier <x.crochet@gmail.com>
2020-07-11 22:55:58 +0200

    feat: (JWT) Add Security JWT

---
+++ src/main/java/com/myapp/property/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.myapp.property.controller;
+
+
+import com.myapp.property.entity.ApplicationUser;
+import com.myapp.property.repository.ApplicationUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    ApplicationUserRepository applicationUserRepository;
+
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
+    @PostMapping("/sign-up")
+    public void signUp(@RequestBody ApplicationUser user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        applicationUserRepository.save(user);
+    }
+}
---
+++ src/main/java/com/myapp/property/repository/ApplicationUserRepository.java
@@ -0,0 +1,8 @@
+package com.myapp.property.repository;
+
+import com.myapp.property.entity.ApplicationUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApplicationUserRepository extends JpaRepository<ApplicationUser, Long> {
+    ApplicationUser findByUsername(String username);
+}
